48fc212bb1b8a059887061e3d9e050f376047df8
So78073 <srpedroyshisuk1@gmail.com>
2025-01-10 17:37:32 -0300

    version stable

--- scripts.js
+++ scripts.js
@@ -1,10 +1,46 @@
-// Seleciona os elementos
+
 const menuToggle = document.getElementById('menu-toggle');
 const navLinks = document.getElementById('nav-links');
 
-// Adiciona o evento de clique
+
 menuToggle.addEventListener('click', () => {
     console.log('Menu toggle clicked');
     navLinks.classList.toggle('active');
-  });
-  
+});
+
+
+const serviceImages = document.querySelectorAll('.serv_div_img');
+const serviceTitle = document.getElementById('service-title');
+const serviceDescription = document.getElementById('service-description');
+
+// Função para selecionar uma imagem
+function selectImage(image) {
+
+    serviceImages.forEach(img => {
+        img.classList.remove('selected');
+        img.classList.add('not-selected');
+    });
+
+
+    image.classList.add('selected');
+    image.classList.remove('not-selected');
+
+
+    const newTitle = image.getAttribute('data-title');
+    const newDescription = image.getAttribute('data-description');
+
+
+    serviceTitle.textContent = newTitle;
+    serviceDescription.textContent = newDescription;
+}
+
+
+serviceImages.forEach(image => {
+    image.addEventListener('click', () => {
+        selectImage(image);
+    });
+});
+
+if (serviceImages.length > 0) {
+    selectImage(serviceImages[0]); 
+}
